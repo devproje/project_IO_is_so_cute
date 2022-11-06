e1e1aa1a770eca4f9eca6191eb0d9d2349faec47
Quema <105044971+Quema100@users.noreply.github.com>
2022-11-06 23:43:39 +0900

    Create Project_TL is so cute.java

---
+++ Project_TL is so cute.java
@@ -0,0 +1,19 @@
+class Main {  
+
+  public static void main(String args[]) { 
+
+int num = 0 ;
+
+    while (num==0)
+
+      {
+
+        System .out.println("쁘젝님 ㄱㅇㅇ..");
+
+      }
+
+    
+
+  } 
+
+}
